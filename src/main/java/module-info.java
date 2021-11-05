3a531685c12b63578dbfa7a984a717bdcf31c1ec
dkwapisz <dkwapisz@student.agh.edu.pl>
2021-11-05 15:39:00 +0100

    Initial files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module io.project.ships {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens io.project.ships to javafx.fxml;
+    exports io.project.ships;
+}
