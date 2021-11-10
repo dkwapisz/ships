ce12d9bf585803240bc324576c0fed103421c040
dkwapisz <dkwapisz@student.agh.edu.pl>
2021-11-10 13:07:34 +0100

    MainBoard FXML + test rectangle

--- src/main/java/io/project/ships/controllers/MainController.java
+++ src/main/java/io/project/ships/controllers/MainController.java
@@ -1,6 +1,37 @@
 package io.project.ships.controllers;
 
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private Pane playerBoard;
+
+    @FXML
+    private Pane enemyBoard;
+
+    @FXML
+    private Rectangle rectangleTest;
+
+    @FXML
+    void backToMenu() throws IOException {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        Pane root = FXMLLoader.load(getClass().getResource("/fxml/menu-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
