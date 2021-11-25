1fcf5ed8fa0de15b92c3772a5d806a181c705b3e
annamlynarczyk <a.mlynarczyk6@gmail.com>
2021-11-25 14:09:59 +0100

    login window style

--- src/main/java/io/project/ships/controllers/LoginWindowController.java
+++ src/main/java/io/project/ships/controllers/LoginWindowController.java
@@ -1,9 +1,12 @@
 package io.project.ships.controllers;
 
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -17,16 +20,32 @@ public class LoginWindowController {
     private AnchorPane anchorPane;
 
     @FXML
+    private Button loginButton;
+
+    @FXML
     private TextField loginTextField;
 
     @FXML
+    private PasswordField passwordTextField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
     void login() throws IOException {
-        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        Stage stage = (Stage) loginTextField.getScene().getWindow();
         Pane root = FXMLLoader.load(getClass().getResource("/fxml/menu-view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
+    @FXML
+    void register() {
+
+    }
 
 }
