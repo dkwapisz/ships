39d0bd23364fa93b11961eadfa28e3a5531249c4
annamlynarczyk <a.mlynarczyk6@gmail.com>
2021-12-01 13:28:52 +0100

    menu style

--- src/main/java/io/project/ships/controllers/MenuController.java
+++ src/main/java/io/project/ships/controllers/MenuController.java
@@ -4,6 +4,9 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,11 +16,26 @@ import java.io.IOException;
 public class MenuController {
 
     @FXML
-    private VBox menuScene;
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button gameSettingsButton;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Button replaysButton;
+
+    @FXML
+    private Button statisticsButton;
+
+    @FXML
+    private Label userLabel;
 
     @FXML
     void goToGameSettings() throws IOException {
-        Stage stage = (Stage) menuScene.getScene().getWindow();
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
         Pane root = FXMLLoader.load(getClass().getResource("/fxml/gameSettings-view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -26,7 +44,7 @@ public class MenuController {
 
     @FXML
     void goToStatistics() throws IOException {
-        Stage stage = (Stage) menuScene.getScene().getWindow();
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
         Pane root = FXMLLoader.load(getClass().getResource("/fxml/statistics-view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -35,7 +53,7 @@ public class MenuController {
 
     @FXML
     void goToReplays() throws IOException {
-        Stage stage = (Stage) menuScene.getScene().getWindow();
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
         Pane root = FXMLLoader.load(getClass().getResource("/fxml/replays-view.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
