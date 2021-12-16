f77db85e3339f7295954d8ef6af88f1cac03398f
Mateusz <maswi0118@gmail.com>
2021-12-16 16:31:41 +0100

    some replays classes and functions;

--- src/main/java/io/project/ships/menu/GameHistory.java
+++ src/main/java/io/project/ships/menu/GameHistory.java
@@ -2,48 +2,30 @@ package io.project.ships.menu;
 
 public class GameHistory {
     private int gid;
-    private int uid1;
-    private int uid2;
+    private int uid;
     private int moveNumber;
-    private String board1;
-    private String board2;
-    private int isAiVsAi;
+    private String board;
 
-    public GameHistory(int gid, int uid1, int uid2, int moveNumber, String board1, String board2, int isAiVsAi){
-        this.gid=gid;
-        this.uid1=uid1;
-        this.uid2=uid2;
-        this.moveNumber=moveNumber;
-        this.board1=board1;
-        this.board2=board2;
-        this.isAiVsAi=isAiVsAi;
+    public GameHistory(int gid, int moveNumber, int uid, String board){
+        this.gid = gid;
+        this.uid = uid;
+        this.moveNumber = moveNumber;
+        this.board = board;
     }
 
     public int getGid() {
         return gid;
     }
 
-    public int getUid1() {
-        return uid1;
-    }
-
-    public int getUid2() {
-        return uid2;
+    public int getUid() {
+        return uid;
     }
 
     public int getMoveNumber() {
         return moveNumber;
     }
 
-    public String getBoard1() {
-        return board1;
-    }
-
-    public String getBoard2() {
-        return board2;
-    }
-
-    public int getIsAiVsAi() {
-        return isAiVsAi;
+    public String getBoard() {
+        return board;
     }
 }
